d9005f666211bdb3021038b0b041bb37294cc7f8
Kashi-R <routraykashinatha@gmail.com>
2026-02-04 23:19:22 +0530

    solve not string

---
+++ Warmup-1/notString/solution.java
@@ -0,0 +1,17 @@
+public String notString(String str) {
+  if(str.startsWith("not"))
+  return str;
+  else 
+  return ("not "+str);
+}
+
+
+//OR
+//actual sloution
+public String notString(String str) {
+  if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+    return str;
+  }
+  
+  return "not " + str;
+}
---
+++ Warmup-1/posNeg/solution.java
@@ -0,0 +1,12 @@
+
+public boolean posNeg(int a, int b, boolean negative) {
+  if(!negative) {
+   if((a<0 && b>0)||(a>0 && b<0))
+    return true;
+  }
+  else if(negative){
+    if(a<0 && b<0)
+     return true;
+  }
+  return false ;
+}
